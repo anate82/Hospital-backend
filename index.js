3ba9703d1004115dc604b13121291cc246e0f42a
ana <anate82@gmail.com>
2021-06-04 11:52:06 +0100

    SPA control

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const path = require('path');
 const cors = require('cors');
 const { dbConnection } = require('./database/config')
 //Crear servidor express
@@ -21,6 +22,10 @@ app.use('/api/hospitales', require('./routes/hospitales.routes'))
 app.use('/api/medicos', require('./routes/medicos.routes'))
 app.use('/api/todo', require('./routes/busquedas.routes'))
 app.use('/api/upload', require('./routes/upload.routes'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
